9c90bd8981bc3906e748a174271bf02f69339bc5
Abhiraj <abhirajsardar2003@gmail.com>
2024-01-08 17:05:47 +0530

    all done

--- app.js
+++ app.js
@@ -6,21 +6,27 @@ app.set("view engine","ejs");
 app.use(express.static("./public"));
 app.use(express.urlencoded({extended:false}));
 
-app.get("/",(req,res)=>{
-    res.render("index");
-})
-
-app.get("/insert",(req,res)=>{
-    res.redirect("/");
+app.get("/",async(req,res)=>{
+    const expenseList = await expenses.find({});
+    console.log(expenseList);
+    res.render("index",{
+        expenseList:expenseList
+    });
 })
 
 app.post("/insert",async(req,res)=>{
     const expense = req.body.expense;
     const amount = req.body.amount;
-    const date = req.body.date;
+    const date = req.body.dop;
     const newExpense = new expenses({expense,amount,date});
     const newExpenseSave = await newExpense.save();
-    res.redirect("index");
+    res.redirect("/");
+})
+
+app.get("/delete/:id",async(req,res)=>{
+    const id = req.params.id;
+    const deleteExpense = await expenses.findByIdAndDelete({_id:id});
+    res.redirect("/");
 })
 
 app.listen(3000,()=>{
